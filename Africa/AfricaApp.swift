746f9bd8d2b07152a8a92824d95e148c62d5580c
Pat Butler <15131675+Beefycoder@users.noreply.github.com>
2022-04-11 12:03:29 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Pat Butler on 2022-04-11.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
